2db105c829eebc71e10aa335927d3e5d44faa154
theosun <theo.sun@outlook.com>
2017-11-03 19:31:51 +0800

    [generated] by spring starter

---
+++ src/main/java/org/fornever/spring/stepbystep/StepbystepApplication.java
@@ -0,0 +1,12 @@
+package org.fornever.spring.stepbystep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepbystepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepbystepApplication.class, args);
+	}
+}
---
+++ src/test/java/org/fornever/spring/stepbystep/StepbystepApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fornever.spring.stepbystep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StepbystepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
