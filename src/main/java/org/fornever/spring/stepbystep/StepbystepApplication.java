be571281212862ef4364ccda54057ef1045815d0
Theo Sun <theo.sun@sap.com>
2017-11-15 11:22:10 +0800

    [add] swagger ui

--- src/main/java/org/fornever/spring/stepbystep/StepbystepApplication.java
+++ src/main/java/org/fornever/spring/stepbystep/StepbystepApplication.java
@@ -2,13 +2,19 @@ package org.fornever.spring.stepbystep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+// enable swagger spring data rest support
+@EnableSwagger2
+@Import({ springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class })
 public class StepbystepApplication {
 
 	// Spring Framework will control the entire program from here
 	public static void main(String[] args) {
 		SpringApplication.run(StepbystepApplication.class, args);
 	}
-	
+
 }
--- src/main/java/org/fornever/spring/stepbystep/services/CompanyService.java
+++ src/main/java/org/fornever/spring/stepbystep/services/CompanyService.java
@@ -6,6 +6,8 @@ import org.fornever.spring.stepbystep.repos.CompanyRepo;
 import org.fornever.spring.stepbystep.repos.ProducrtRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
